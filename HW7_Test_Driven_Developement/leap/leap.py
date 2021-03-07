283f3f05d94f2995c1df3d06e02930c736096529
Keeganfn <keegan.nave@gmail.com>
2021-03-07 15:29:44 -0800

    initial files for leap program

---
+++ HW7_Test_Driven_Developement/leap/leap.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+#Keegan Nave 
+#tested with python3 and linux
+
+
+
+if __name__ == "__main__":
+   pass 
---
+++ HW7_Test_Driven_Developement/leap/test_leap.py
@@ -0,0 +1,9 @@
+import unittest
+import leap
+
+class TestFunctions(unittest.TestCase):
+    def test1(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
