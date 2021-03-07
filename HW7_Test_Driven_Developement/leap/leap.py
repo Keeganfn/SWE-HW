4ca391ae56fd5f9a2edfb5c40c130f674a8b572d
Keeganfn <keegan.nave@gmail.com>
2021-03-07 15:46:22 -0800

    Wrote 1st test, checks to see if div4 returns True if number is evenly divisible by 4 and false if it isnt

--- HW7_Test_Driven_Developement/leap/leap.py
+++ HW7_Test_Driven_Developement/leap/leap.py
@@ -3,6 +3,8 @@
 #Keegan Nave 
 #tested with python3 and linux
 
+def div4(number):
+    pass
 
 
 if __name__ == "__main__":
--- HW7_Test_Driven_Developement/leap/test_leap.py
+++ HW7_Test_Driven_Developement/leap/test_leap.py
@@ -2,7 +2,9 @@ import unittest
 import leap
 
 class TestFunctions(unittest.TestCase):
-    def test1(self):
+    def test_div4(self):
+        assert leap.div4(4) == True
+        assert leap.div4(5) == False
         pass
 
 if __name__ == '__main__':
